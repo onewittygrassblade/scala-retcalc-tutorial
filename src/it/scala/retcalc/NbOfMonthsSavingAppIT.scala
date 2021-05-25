170bd7a024f1b6673aabc3a460c4b9c3d163aecc
OneWittyGrassBlade <jillphys@hotmail.com>
2021-05-25 14:50:12 +0100

    Implement NbOfMonthsSavingApp

---
+++ src/it/scala/retcalc/NbOfMonthsSavingAppIT.scala
@@ -0,0 +1,18 @@
+package retcalc
+
+import org.scalactic.TypeCheckedTripleEquals
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class NbOfMonthsSavingAppIT extends AnyWordSpec with Matchers with TypeCheckedTripleEquals {
+  "NbOfMonthsSavingApp.strMain" should {
+    "simulate a retirement plan using market returns" in {
+      val actual = NbOfMonthsSavingApp.strMain(
+        Array("1997.09,2017.09", "40", "3000", "2000", "10000")
+      )
+
+      val expected = "Time needed to save before retirement: 252 months (21 years)"
+      actual should ===(expected)
+    }
+  }
+}
---
+++ src/main/scala/retcalc/NbOfMonthsSavingApp.scala
@@ -0,0 +1,27 @@
+package retcalc
+
+object NbOfMonthsSavingApp extends App {
+  println(strMain(args))
+
+  def strMain(args: Array[String]): String = {
+    val (from +: until +: Nil) = args(0).split(",").toList
+    val nbOfYearsInRetirement  = args(1).toInt
+
+    val returns = Returns.fromEquityAndInflationData(
+      EquityData.fromResource("sp500.tsv"),
+      InflationData.fromResource("cpi.tsv")
+    )
+
+    val nbOfMonthsSaving = RetCalc.nbOfMonthsSaving(
+      returns = returns.fromUntil(from, until),
+      params = RetCalcParams(
+        nbOfMonthsInRetirement = nbOfYearsInRetirement * 12,
+        netIncome = args(2).toInt,
+        monthlyExpenses = args(3).toInt,
+        initialCapital = args(4).toInt
+      )
+    )
+
+    s"Time needed to save before retirement: $nbOfMonthsSaving months (${nbOfMonthsSaving / 12} years)"
+  }
+}
